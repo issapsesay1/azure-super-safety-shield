5780fa9fcd0ed8727a26e454d37818daace24652
issapsesay1 <issabsesay@gmail.com>
2026-08-09 10:54:01 -0400

    Add conditions to conditional access policy

--- terraform/conditional_access.tf
+++ terraform/conditional_access.tf
@@ -1,17 +1,21 @@
-hcl
 resource "azuread_conditional_access_policy" "stop_bad_guys" {
   display_name = "Stop Bad Guys - High Risk Block"
   state        = "enabled"
+
   conditions {
     client_app_types = ["all"]
+
     applications {
       included_applications = ["All"]
     }
+
     users {
       included_users = ["All"]
     }
+
     user_risk_levels = ["high", "medium"]
   }
+
   grant_controls {
     operator          = "OR"
     built_in_controls = ["block"]
